dc4b86ae0653369fd4996b3d7750fc77e856dff8
topzz100 <adenekantope@gmail.com>
2022-01-16 14:38:00 +0100

    multer set up

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const userRoute = require('./routes/users')
 const authRoute = require('./routes/auths')
 const postRoute = require('./routes/posts')
 const categoryRoute = require('./routes/categories')
+const multer = require("multer");
+const path = require("path");
 
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const connectDB = async(url) => {
   try{
@@ -21,6 +24,20 @@ const connectDB = async(url) => {
   
 }
 connectDB(process.env.MONGO_URI)
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
