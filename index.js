88358ff288c90f2da37d866edc2086f7f6e8a9f8
topzz100 <adenekantope@gmail.com>
2022-02-02 17:07:20 +0100

    +++

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const categoryRoute = require('./routes/categories')
 const multer = require("multer");
 const path = require("path");
 
-app.use(cors())
+app.use(cors({ origin: '*', credentials: true}))
 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
